c71394d23156728e0357d1c1939200a0905d49c2
Christoph Welcz <christoph@welcz.de>
2019-03-01 00:55:53 +0100

    quickcheck and all warnings turned on

--- mars-rover/test/MarsRoverSpec.hs
+++ mars-rover/test/MarsRoverSpec.hs
@@ -4,10 +4,6 @@ module MarsRoverSpec
 
 import           MarsRover
 import           Test.Hspec        (Spec, context, describe, it, shouldBe)
-import           Test.Hspec.Runner (hspec)
-
-main :: IO ()
-main = hspec spec
 
 spec :: Spec
 spec = describe "Mars Rover" $
