836ab3423040a5b758bd5bf61f05efe84c56bffd
Christoph Welcz <christoph@welcz.de>
2019-03-04 23:35:13 +0100

    made lambda params longer + removed empty rover in favor of constructor method

--- mars-rover/test/MarsRoverSpec.hs
+++ mars-rover/test/MarsRoverSpec.hs
@@ -25,36 +25,36 @@ isValidCommand = (`elem` "flbr")
 spec :: Spec
 spec =
   describe "Mars Rover" $ do
-    let rover = mkRover
     context "initialization" $ do
-      it "has a default position" $ position rover `shouldBe` (0, 0)
-      it "has a default orientation" $ orientation rover `shouldBe` North
+      it "has a default position" $ position mkRover `shouldBe` (0, 0)
+      it "has a default orientation" $ orientation mkRover `shouldBe` North
     context "single commands" $ do
       it "moves forward by one field according to its orientation" $ do
-        forward rover `shouldBe` rover {position = (0, 1)}
-        forward rover {position = (4, 2)} `shouldBe` rover {position = (4, 3)}
-        forward rover {orientation = West} `shouldBe` rover {position = (1, 0), orientation = West}
-        forward rover {orientation = South} `shouldBe` rover {position = (0, -1), orientation = South}
-        forward rover {orientation = East} `shouldBe` rover {position = (-1, 0), orientation = East}
+        forward mkRover `shouldBe` mkRover {position = (0, 1)}
+        forward mkRover {position = (4, 2)} `shouldBe` mkRover {position = (4, 3)}
+        forward mkRover {orientation = West} `shouldBe` mkRover {position = (1, 0), orientation = West}
+        forward mkRover {orientation = South} `shouldBe` mkRover {position = (0, -1), orientation = South}
+        forward mkRover {orientation = East} `shouldBe` mkRover {position = (-1, 0), orientation = East}
       it "turns left by 90 degrees" $ do
-        turnLeft rover `shouldBe` rover {orientation = West}
-        turnLeft rover {orientation = West} `shouldBe` rover {orientation = South}
-        turnLeft rover {orientation = South} `shouldBe` rover {orientation = East}
-        turnLeft rover {orientation = East} `shouldBe` rover {orientation = North}
+        turnLeft mkRover `shouldBe` mkRover {orientation = West}
+        turnLeft mkRover {orientation = West} `shouldBe` mkRover {orientation = South}
+        turnLeft mkRover {orientation = South} `shouldBe` mkRover {orientation = East}
+        turnLeft mkRover {orientation = East} `shouldBe` mkRover {orientation = North}
       it "turns right by 90 degrees" $ property $ \r -> turnRight r `shouldBe` (turnLeft . turnLeft . turnLeft) r
       it "moves back by one field according to its orientation" $
         property $ \r -> backward r `shouldBe` (turnLeft . turnLeft . forward . turnLeft . turnLeft) r
     context "translating Char -> command" $ do
-      it "moves forward on 'f'" $ property $ \r -> command 'f' r `shouldBe` forward <$> Just r
-      it "moves backward on 'b'" $ property $ \r -> command 'b' r `shouldBe` backward <$> Just r
-      it "turns left on 'l'" $ property $ \r -> command 'l' r `shouldBe` turnLeft <$> Just r
-      it "turns right on 'r'" $ property $ \r -> command 'r' r `shouldBe` turnRight <$> Just r
+      it "moves forward on 'f'" $ property $ \rover -> command 'f' rover `shouldBe` forward <$> Just rover
+      it "moves backward on 'b'" $ property $ \rover -> command 'b' rover `shouldBe` backward <$> Just rover
+      it "turns left on 'l'" $ property $ \rover -> command 'l' rover `shouldBe` turnLeft <$> Just rover
+      it "turns right on 'r'" $ property $ \rover -> command 'r' rover `shouldBe` turnRight <$> Just rover
     context "multiple commands" $ do
-      it "moves forward twice" $ commands rover "ff" `shouldBe` rover {position = (0, 2)}
+      it "moves forward twice" $ commands mkRover "ff" `shouldBe` mkRover {position = (0, 2)}
       it "executes multiple commands" $
-        commands rover "fflllrrbbbbff" `shouldBe` rover {position = (-2, 2), orientation = West}
+        commands mkRover "fflllrrbbbbff" `shouldBe` mkRover {position = (-2, 2), orientation = West}
     context "ignoring errors in input" $ do
       it "ignores invalid single command" $
-        property $ forAll (suchThat arbitrary isInvalidCommand) $ \c r -> command c r `shouldBe` Nothing
+        property $ forAll (suchThat arbitrary isInvalidCommand) $ \char rover -> command char rover `shouldBe` Nothing
       it "ignores invalid list of commands" $
-        property $ forAll (suchThat arbitrary containsBothValidAndInvalidCommands) $ \cs r -> commands r cs `shouldBe` r
+        property $ forAll (suchThat arbitrary containsBothValidAndInvalidCommands) $
+          \list rover -> commands rover list `shouldBe` rover
